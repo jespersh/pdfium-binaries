0665befc2b9df2211cc091599415d7a49e48ed57
Jesper Staun Hansen <jsh@aldente.dk>
2021-06-01 13:58:47 +0200

    Remove accidental submodules

--- build.bat
+++ build.bat
@@ -1,10 +1,3 @@
-set GYP_MSVS_VERSION=2019
-set PDFium_BRANCH=chromium/4522
-set PLATFORM=x64
-set PDFium_V8=enabled
-set CONFIGURATION=Debug
-set PATH=%PATH%;C:\Program Files\7-Zip\
-
 : Variables to provide:
 : GYP_MSVS_VERSION = 2019 | 2017 | 2015
 : CONFIGURATION = Debug | Release
