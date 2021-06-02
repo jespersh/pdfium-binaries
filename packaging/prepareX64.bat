058668febfd72c70e2ae34cc83a7fe9430e6919e
Jesper Staun Hansen <jsh@aldente.dk>
2021-06-02 14:12:56 +0200

    nuget publishing

---
+++ packaging/prepareX64.bat
@@ -0,0 +1,4 @@
+mkdir %~dp0win-x64\content\x64\
+copy .\staging\x64\bin\pdfium.dll %~dp0win-x64\content\x64\
+copy .\staging\x64\res\icudtl.dat %~dp0win-x64\content\x64\
+copy .\staging\x64\res\snapshot_blob.bin %~dp0win-x64\content\x64\
---
+++ packaging/prepareX86.bat
@@ -0,0 +1,4 @@
+mkdir %~dp0win-x86\content\x86\
+copy .\staging\x86\bin\pdfium.dll %~dp0win-x86\content\x86\
+copy .\staging\x86\res\icudtl.dat %~dp0win-x86\content\x86\
+copy .\staging\x86\res\snapshot_blob.bin %~dp0win-x86\content\x86\
